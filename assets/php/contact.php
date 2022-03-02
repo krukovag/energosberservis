9175f4dab30e9ea4ac3615020847c1b336d66b47
krukovag <krukovag@ya.ru>
2022-03-02 11:13:21 +0300

    initial

---
+++ assets/php/contact.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['message'])){
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+    
+	
+	$to      = 'krukovag@ya.ru';
+	$subject = 'Сообщение с сайта Инжиниринговой компании';
+
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: '. $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+	$status = mail($to, $subject, $message, $headers);
+
+	if($status == TRUE){
+		$res['sendstatus'] = 'done';
+	
+		//Edit your message here
+		$res['message'] = 'Form Submission Successful';
+    }
+	else{
+		$res['message'] = 'Failed to send mail. Please mail me to krukovag@ya.ru';
+	}
+	
+	
+	echo json_encode($res);
+}
+
+?>
